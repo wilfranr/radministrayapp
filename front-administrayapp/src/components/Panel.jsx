325defa8f3a8120c277e019f5f9c22a6e1e4a288
wilfranr <wilfranr@gmail.com>
2021-11-27 08:54:28 -0500

    Register funcioanl

--- front-administrayapp/src/components/Panel.jsx
+++ front-administrayapp/src/components/Panel.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import LogoClasificados from "../images/Clasificados.png";
 import LogoComunicados from "../images/Comunicados.png";
 import LogoContacto from "../images/Contacto.png";
@@ -16,42 +17,25 @@ import "../styles/style.css"
 function Panel(props) {
     return (
         <section id="panel">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-lg-4">
-                        <a href="/clasificados">
-                            <img src={LogoClasificados} alt="clasificados" width="140" height="140" element={Clasificados} />
-                        </a>
-                    </div>
-                    <div className="col-lg-4">
-                        <a href="/comunicados">
-                            <img src={LogoComunicados} alt="comunicados" width="140" height="140" component={Comunicados} />
-                        </a>
-                    </div>
-                    <div className="col-lg-4">
-                        <a href="/contacto">
-                            <img src={LogoContacto} alt="contacto" width="140" height="140" component={Contacto} />
-                        </a>
-                    </div>
-                </div>
-                <div className="row mt-5">
-                    <div className="col-lg-4">
-                        <a href="/estadocuenta">
-                        
-                            <img src={LogoEstadoCuenta} alt="estado de cuenta" width="140" height="140" component={EstadoCuenta} />
-                        </a>
-                    </div>
-                    <div className="col-lg-4">
-                        <a href="/emprendimientos">
-                            <img src={LogoEmprendimientos} alt="emprendimientos" width="140" height="140" component={Emprendimientos} />
-                        </a>
-                    </div>
-                    <div className="col-lg-4">
-                        <a href="/acerca">
-                            <img src={LogoAcerca} alt="acerca" width="140" height="140" component={Acerca} />
-                        </a>
-                    </div>
-                </div>
+            <div class="grid">
+                <Link to="/clasificados" style={{color: 'white'}}>
+                    <img src={LogoClasificados} alt="clasificados" width="140" height="140" component={Clasificados}/>
+                </Link>
+                <Link to="/comunicados" style={{color: 'white'}}>
+                    <img src={LogoComunicados} alt="comunicados" width="140" height="140" component={Comunicados} />
+                </Link>
+                <Link to="/contacto" style={{color: 'white'}}>
+                    <img src={LogoContacto} alt="contacto" width="140" height="140" component={Contacto} />
+                </Link>
+                <Link to="/estadocuenta" style={{color: 'white'}}>
+                    <img src={LogoEstadoCuenta} alt="estado de cuenta" width="140" height="140" component={EstadoCuenta} />
+                </Link>
+                <Link to="/emprendimientos" style={{color: 'white'}}>
+                    <img src={LogoEmprendimientos} alt="emprendimientos" width="140" height="140" component={Emprendimientos} />
+                </Link>
+                <Link to="/acerca" style={{color: 'white'}}>
+                    <img src={LogoAcerca} alt="acerca" width="140" height="140" component={Acerca} />
+                </Link>
             </div>
         </section>
     )
--- front-administrayapp/src/views/Acerca.jsx
+++ front-administrayapp/src/views/Acerca.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer"
+import Slide from "../components/Slide";
 
 function Acerca() {
     return (
         <div>
             <Header></Header>
+            <Slide></Slide>
             <Footer></Footer>
         </div>
     )
 }
+
 export default Acerca
