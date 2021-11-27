837b67fa6ea3d1f4d85108afcfceebeaa49b955d
jvargas18 <bugster_18@hotmail.com>
2021-11-27 09:29:45 -0500

    Crud clasificados
    Se añade crud de clasificados back y front.

--- front-administrayapp/src/views/Clasificados.jsx
+++ front-administrayapp/src/views/Clasificados.jsx
@@ -1,12 +1,34 @@
-import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer"
 import anuncio from "../images/anuncio.jpg"
 import anuncio2 from "../images/anuncio2.jpg"
 
+import React, {useEffect, useState, useRef} from 'react'
+import axios from 'axios'
+
 
 function Clasificados() {
+    
+    const [classifieds,setClassifieds]=useState([])
+
+    useEffect(()=>{
+        obtenerClasificados()
+    },[])
+
+    const obtenerClasificados= async()=>{
+            const respuesta= await axios.get(process.env.REACT_APP_API_URL+'classified')
+            //console.log(respuesta.data)
+            setClassifieds(respuesta.data)
+        }
+
+    const eliminarClasificados= async(id)=>{
+        //console.log(id)
+        await axios.delete(process.env.REACT_APP_API_URL+'classified/'+id)
+        obtenerClasificados()
+    }
+
     return (
+        
         <div>
             <Header></Header>
             <div class="clasificados">
@@ -15,44 +37,32 @@ function Clasificados() {
                 <a href="/NuevoClasificado">
                     <button class="btn btn-primary" type="button">Nuevo</button>
                 </a><br /><br />
-                <div class="cards">
-                    <div class="card" style={{ width: "18rem" }}>
-                        <img src={anuncio} class="card-img-top" alt="" />
-                        <div class="card-body bg-light">
-                            <h5 class="card-title">SE ARRIENDA</h5>
-                            <p class="card-text">
-                                APARTAMENTO AMOBLADO Apartamenton con Buenos Acabados Cerca de
-                                Centros Comerciales...
-                            </p>
-                            <a href="https://www.fincaraiz.com.co/proyecto-de-vivienda/moka/pance/cali/6486882"
-                                class="btn btn-primary">Ver anuncio
-                            </a>
-                        </div>
-                    </div>
-                    <div class="card" style={{ width: "18rem" }}>
-                        <img src={anuncio2} class="card-img-top" alt="" />
-                        <div class="card-body bg-light">
-                            <h5 class="card-title">SE VENDE</h5>
-                            <p class="card-text">
-                                Exclusivo diseño con aprovechamiento total de luz y una magnifica
-                                vista desde la terraza solárium, a las montañas orientales...
-                            </p>
-                            <a href="#!" class="btn btn-primary">Ver anuncio
-                            </a>
-                        </div>
-                    </div>
-                    <div class="card" style={{ width: "18rem" }}>
-                        <img src={anuncio} class="card-img-top" alt="" />
-                        <div class="card-body bg-light">
-                            <h5 class="card-title">SE ARRIENDA</h5>
-                            <p class="card-text">
-                                APARTAMENTO AMOBLADO Apartamenton con Buenos Acabados Cerca de
-                                Centros Comerciales...
-                            </p>
-                            <a href="#!" class="btn btn-primary">Ver anuncio
-                            </a>
-                        </div>
-                    </div>
+                                          
+                <div className="cards">
+                    {                        
+                        classifieds.map(classified => (
+                            <div className="col-md-10 p-2" key={classified._id}>
+                                <div className="card">
+                                    <div className="card-header d-flex justify-content-between" heig>
+                                        <h5>{classified.nombre.substring(0, 10)+"..."}</h5>
+                                        <div >
+                                            <a href={"/editarclasificado/"+classified._id}>
+                                                <button className="btn btn-secondary" type="button">Edit</button>
+                                            </a>
+                                            
+                                            <button className="btn btn-danger" onClick={() => {if(window.confirm('Esta seguro de eliminar el clasificado?')){ eliminarClasificados(classified._id)};}}>Del</button>
+                                        </div>
+                                        
+                                    </div>
+                                    <div className="card-body" width="18rem;">
+                                        <img src={`${process.env.REACT_APP_API_URL}/${classified.image}`} width="100%" height="200" class="card-img-top" />
+                                        <p className="card-text" style={{ color: 'white' }}>{classified.descripcion.substring(0, 100)+"..."}</p>
+                                         <a href={classified.url} class="btn btn-primary">Ver anuncio</a>
+                                    </div>     
+                                </div>
+                            </div>
+                        ))                        
+                    }
                 </div>
             </div>
             <Footer></Footer>
